d30b3d12bee5971d769726074f86a6e45f82a9f3
Martin Macharia <mosmartin5@gmail.com>
2024-02-20 10:57:15 +0100

    Refactor the event functions

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,11 @@
 import * as dotenv from "dotenv";
 import { ethers } from "ethers";
-import { BlockTag } from "@ethersproject/abstract-provider";
 
 import feeCollectorABI from "../abi/fee_collector_abi.json";
 import connectToDatabase from "./db/db";
+import { getEvents, parseFeeCollectorEvent } from "./utils/Utils";
 
 dotenv.config();
-
-interface ParsedFeeCollectedEvent {
-  token: string;
-  integrator: string;
-  integratorFee: BigInt;
-  lifiFee: BigInt;
-  blockNumber: bigint;
-}
 
 const contractAddress = "0xbD6C7B0d2f68c2b7805d88388319cfB6EcB50eA9";
 const provider = new ethers.JsonRpcProvider(`${process.env.POLYGON_RPC_URL}`);
@@ -24,40 +16,18 @@ const feeCollector = new ethers.Contract(
   provider
 );
 
-const getEvents = async (
-  fromBlock: BlockTag,
-  toBlock: BlockTag
-): Promise<(ethers.Log | ethers.EventLog)[]> => {
-  const filter = feeCollector.filters.FeesCollected();
-  return feeCollector.queryFilter(filter, fromBlock, toBlock);
-};
-
-const parseFeeCollectorEvent = (
-  event: (ethers.Log | ethers.EventLog)[]
-): ParsedFeeCollectedEvent[] => {
-  return event.map((e) => {
-    const parsedEvent = feeCollector.interface.parseLog(e);
-
-    const parsed: ParsedFeeCollectedEvent = {
-      token: parsedEvent?.args[0],
-      integrator: parsedEvent?.args[1],
-      integratorFee: parsedEvent?.args[2],
-      lifiFee: parsedEvent?.args[3],
-      blockNumber: BigInt(e.blockNumber),
-    };
-
-    return parsed;
-  });
-};
-
 const main = async () => {
   connectToDatabase();
 
   try {
     const currentBlock = await provider.getBlockNumber();
 
-    const events = await getEvents(blockNumber, blockNumber + 1000);
-    const parsedEvents = parseFeeCollectorEvent(events);
+    const events = await getEvents(
+      blockNumber,
+      blockNumber + 1000,
+      feeCollector
+    );
+    const parsedEvents = parseFeeCollectorEvent(events, feeCollector);
     console.log(currentBlock);
     console.log(parsedEvents);
   } catch (error) {
---
+++ src/utils/Utils.ts
@@ -0,0 +1,38 @@
+import { ethers } from "ethers";
+import { BlockTag } from "@ethersproject/abstract-provider";
+
+interface ParsedFeeCollectedEvent {
+  token: string;
+  integrator: string;
+  integratorFee: BigInt;
+  lifiFee: BigInt;
+  blockNumber: bigint;
+}
+
+export const getEvents = async (
+  fromBlock: BlockTag,
+  toBlock: BlockTag,
+  feeCollector: ethers.Contract
+): Promise<(ethers.Log | ethers.EventLog)[]> => {
+  const filter = feeCollector.filters.FeesCollected();
+  return feeCollector.queryFilter(filter, fromBlock, toBlock);
+};
+
+export const parseFeeCollectorEvent = (
+  event: (ethers.Log | ethers.EventLog)[],
+  feeCollector: ethers.Contract
+): ParsedFeeCollectedEvent[] => {
+  return event.map((e) => {
+    const parsedEvent = feeCollector.interface.parseLog(e);
+
+    const parsed: ParsedFeeCollectedEvent = {
+      token: parsedEvent?.args[0],
+      integrator: parsedEvent?.args[1],
+      integratorFee: parsedEvent?.args[2],
+      lifiFee: parsedEvent?.args[3],
+      blockNumber: BigInt(e.blockNumber),
+    };
+
+    return parsed;
+  });
+};
